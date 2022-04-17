a7d454b386058517abb808d65296853a88c8fb49
Юлия <uliastekolnikova12822@gmail.com>
2022-04-17 22:37:25 +0500

    Исправления: Файл с тестами разнесли на 2 (1 для класса и 1 для апи) Все докстринги перевели в lower case (с заглавной буквы). Без капса.

---
+++ CW_2/config.py
@@ -0,0 +1,4 @@
+POSTS_JSON = 'data/posts.json'  # Файл с постами
+COMMENTS_JSON = 'data/comments.json'  # Файл с комментами
+BOOKMARKS_JSON = 'data/bookmarks.json'  # Файл с закладками
+STRIP_SYM_IN_TAG = '!?,.@'  # Символы для "обрезания" при поиски хештегов
