3735c78ae59d8991b2c2e4c0e8cf4a04a86e5b7a
Julia-Gareeva <101513352+Julia-Gareeva@users.noreply.github.com>
2022-06-12 21:19:55 +0500

    Delete CW_2 directory

--- CW_2/config.py
+++
@@ -1,4 +0,0 @@
-POSTS_JSON = 'data/posts.json'  # Файл с постами
-COMMENTS_JSON = 'data/comments.json'  # Файл с комментами
-BOOKMARKS_JSON = 'data/bookmarks.json'  # Файл с закладками
-STRIP_SYM_IN_TAG = '!?,.@'  # Символы для "обрезания" при поиски хештегов
